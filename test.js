3222ebd38b790a2ec5c73c56d48a60c30089bf81
eddieliu-dev <el3798@nyu.edu>
2026-02-02 13:52:25 -0500

    First commit

---
+++ test.js
@@ -0,0 +1,6 @@
+document.getElementById("show").addEventListener("click", function() {
+    document.getElementById("google").style.display = "block";
+});
+document.getElementById("hide").addEventListener("click", function() {
+    document.getElementById("google").style.display = "none";
+});
