6eb077987b5936f1f764ec4b0b0eb8af8aa4fa78
eddieliu-dev <el3798@nyu.edu>
2026-02-03 12:18:33 -0500

    More updates

--- test.js
+++ test.js
@@ -4,3 +4,6 @@ document.getElementById("show").addEventListener("click", function() {
 document.getElementById("hide").addEventListener("click", function() {
     document.getElementById("google").style.display = "none";
 });
+document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("image").classList.toggle = "hidden";
+});
